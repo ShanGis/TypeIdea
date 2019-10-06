464700e2ddba696c9e1a301c7e85e0c5c5a926a4
WCJ <583306676@qq.com>
2019-10-06 11:24:32 +0800

    增加分类导航

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -7,14 +7,8 @@ from .models import Post, Category, Tag
 
 def post_list(request, category_id=None, tag_id=None):
     queryset = Post.objects.all()
-
-    page = request.GET.get('page', 1)
-    page_size = 4
-    try:
-        page = int(page)
-    except TypeError:
-        page = 1
-
+    
+    # 分类、标签视图
     if category_id:
         queryset = queryset.filter(category_id=category_id)
     elif tag_id:
@@ -24,19 +18,42 @@ def post_list(request, category_id=None, tag_id=None):
             queryset = []
         else:
             queryset = tag.posts.all()
-
+    
+    # 分页
+    page = request.GET.get('page', 1)
+    page_size = 4
+    try:
+        page = int(page)
+    except TypeError:
+        page = 1
     paginator = Paginator(queryset, page_size)
     try:
         posts = paginator.page(page)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-   
+    
+    # 分类导航
+    cates = Category.objects.filter(status=1)
+    nav_cates = []
+    not_nav_cates = []
+    for cate in cates:
+        if cate.is_nav:
+            nav_cates.append(cate)
+        else:
+            not_nav_cates.append(cate)
+    
 
+    # 模板内容
     context = {
-        'posts': posts
+        'posts': posts,
+        'nav_cates': nav_cates,
+        'not_nav_cates': not_nav_cates,
     }
     return render(request, 'blog/list.html', context=context)
 
+    
+
+    
 
 def post_detail(request, pk=None):
     try:
