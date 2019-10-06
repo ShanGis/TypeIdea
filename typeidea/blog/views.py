351a16d80c3aebe698776c3515c242dde64ae6b8
WCJ <583306676@qq.com>
2019-10-06 09:33:52 +0800

    完成简单的页面展示，并做了分页配置

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,3 +1,50 @@
+from django.core.paginator import Paginator,EmptyPage
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
 
-# Create your views here.
+from .models import Post, Category, Tag
+
+
+def post_list(request, category_id=None, tag_id=None):
+    queryset = Post.objects.all()
+
+    page = request.GET.get('page', 1)
+    page_size = 4
+    try:
+        page = int(page)
+    except TypeError:
+        page = 1
+
+    if category_id:
+        queryset = queryset.filter(category_id=category_id)
+    elif tag_id:
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except tag.DoesNotExist:
+            queryset = []
+        else:
+            queryset = tag.posts.all()
+
+    paginator = Paginator(queryset, page_size)
+    try:
+        posts = paginator.page(page)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+   
+
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/list.html', context=context)
+
+
+def post_detail(request, pk=None):
+    try:
+        queryset = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        raise Http404('好像没有你要找的文章呐')
+
+    context = {
+        'post': queryset
+    }
+    return render(request, 'blog/detail.html', context=context)
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -1,24 +1,18 @@
-"""typeidea URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+
 from .custom_site import post_admin_site, comment_admin_site, config_admin_site
+from blog.views import post_list, post_detail
+from config.views import links
 
 
 urlpatterns = [
+    path('', post_list, name='index'),
+    # path('post/', post_detail),
+    path('post/<int:pk>/', post_detail, name='detail'),
+    path('category/<int:category_id>/', post_list, name='category'),
+    path('tag/<int:tag_id>/', post_list, name='tag'),
+    path('links/', links, name='links'),
     path('admin/', admin.site.urls),
     path('post_admin/', post_admin_site.urls),
     path('comment_admin/', comment_admin_site.urls),
