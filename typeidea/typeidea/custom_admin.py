ee41ef6e41881b82260dcfcf6b2c8c8ad167a01c
WuGis <583306676@qq.com>
2019-10-15 19:41:57 +0800

    引入Xadmin之前对已有软件进行一次备份

--- typeidea/typeidea/custom_admin.py
+++ typeidea/typeidea/custom_admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 
 class BaseOwnerAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
-        qs = super(BaseOwnerAdmin, self).get_queryset(request)
+        qs = super().get_queryset(request)
         return qs.filter(owner=request.user)
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
-        return super(BaseOwnerAdmin, self).save_model(request, obj, form, change)
+        return super().save_model(request, obj, form, change)
